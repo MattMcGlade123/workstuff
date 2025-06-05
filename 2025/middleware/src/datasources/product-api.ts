5a2226b277fcbcf6d1030cf15d8863bc48371d33
Matt McGlade <matt.mcglade@kurtgeiger.com>
2025-06-05 09:20:24 +0100

    Add working resolver for aProduct

--- 2025/middleware/src/datasources/product-api.ts
+++ 2025/middleware/src/datasources/product-api.ts
@@ -1,11 +1,15 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { ProductListType } from "../types";
+import { Product, ProductListType } from "../types";
 
 export class ProductAPI extends RESTDataSource {
   baseURL = "http://localhost:8080/";
 
   getProductList(): Promise<ProductListType> {
     return this.get<ProductListType>("products");
+  }
+
+  getAProduct(productSlug: string): Promise<ProductListType> {
+    return this.get<ProductListType>(`products/${productSlug}`);
   }
 
 }
--- 2025/middleware/src/resolvers.ts
+++ 2025/middleware/src/resolvers.ts
@@ -1,6 +1,7 @@
 import { Resolvers } from "./types";
 import { validateFullAmenities } from "./helpers";
 
+
 export const resolvers: Resolvers = {
   Query: {
     featuredListings: (_, __, { dataSources }) => {
@@ -11,6 +12,18 @@ export const resolvers: Resolvers = {
     },
     productList: (_, __, { dataSources }) => {
       return dataSources.productAPI.getProductList();
+    },
+    aProduct: (_, { productSlug }, { dataSources }) => {
+      return dataSources.productAPI.getAProduct(productSlug).then((response) => {
+        const generateId = () => Math.random().toString(36).slice(2);
+        const thisProduct = response.products[0];
+
+        console.log('response', response)
+        return {
+          ...thisProduct,
+          _id: thisProduct._id || generateId()
+        }
+      });
     },
   },
   Product: {
